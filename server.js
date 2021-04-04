7e3fbd8b2121683df9678a877fbe67f51b42cafa
mobilecosta <mobile.costa@gmail.com>
2021-04-04 09:31:25 -0300

    Ajuste Heroku

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
-//Importa as dependências que acabamos de instalar
+//Install express server
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
-// Serve os arquivos estáticos da pasta dist (gerada pelo ng build)
-app.use(express.static(__dirname + '/dist/angularapp'));
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/protheus'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/angularapp/index.html'));
-});
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/protheus/'}),
+);
 
-// Inicia a aplicação pela porta configurada
-app.listen(process.env.PORT || 4200);
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080); 
