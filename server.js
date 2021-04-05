30f98bf6d84f5dde74a68cc5d27c29eb864ae049
mobilecosta <mobile.costa@gmail.com>
2021-04-04 22:06:24 -0300

    Ajuste execução

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
-//Install express server
+//Importa as dependências que acabamos de instalar
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/protheus'));
+// Serve os arquivos estáticos da pasta dist (gerada pelo ng build)
+app.use(express.static(__dirname + '/dist'));
 
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/protheus/'}),
-);
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/index.html'));
+});
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080); 
+// Inicia a aplicação pela porta configurada
+app.listen(process.env.PORT || 4200);
