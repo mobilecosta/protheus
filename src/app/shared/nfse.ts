77aa92b8e68af8b6a60d632136571627d7c66418
guilhermeforprojeto <guylhermesantos@hotmail.com>
2022-08-03 10:28:59 -0300

    interface NFS-e

--- src/app/shared/nfse.ts
+++ src/app/shared/nfse.ts
@@ -1,133 +1,162 @@
 export interface Nfse {
-  indexOf(cliente: any): any;
-  slice(arg0: any, arg1: number);
-  status: string;
-  numero: string;
-  declaracao_prestacao_servico: DeclaracaoServico;
+  id: string
+  created_at: "2022-08-02T19:58:36.515Z"
+  status: "processando"
+  numero: string
+  codigo_verificacao: string
+  link_url: string
+  data_emissao: "2022-08-02T19:58:36.515Z"
+  ambiente: "homologacao"
+  referencia: string
+
+  declaracao_prestacao_servico: Declaracao_prestacao_servico
+  cancelamento: Cancelamento
+  mensagens: Mensagens
 }
 
-export interface DeclaracaoServico {
-  competencia: string;
-  natureza_tributacao: string;
-  fone: string;
+export interface Declaracao_prestacao_servico {
+  rps: Rps
 
-  rps: Rps;
+  competencia: "2022-08-02"
+  natureza_tributacao: 0
 
-  tomador: Tomador;
-
-  prestador: Prestador;
+  preprestador: Prestador
+  tomador: Tomador
+  intermediario: Intermediario
+  construcao_civil: Construcao_civil
+  servicos: Servicos
 }
+
 export interface Rps {
-  identificacao_rps: {
-    numero: number;
-    serie: string;
-  };
-  data_emissao: string;
+  identificacao_rps: Identificacao_rps
+  data_emissao: "2022-08-02T19:58:36.516Z"
+
 }
 
-export interface Tomador {
-  cpf_cnpj: string;
-  inscricao_municipal: string;
-  nome_razao_social: string;
-  fone: string;
-  email: string;
+export interface Identificacao_rps {
+  numero: string
+  serie: string
+  tipo: string
 }
 
 export interface Prestador {
-  cpf_cnpj: string;
-  email: string;
-  nome_fantasia: string;
-  nome_razao_social: string;
+  cpf_cnpj: string
+  inscricao_municipal: string
+  nome_razao_social: string
+  nome_fantasia: string
+  fone: string
+  email: string
+
+  endereco: EnderecoPrestador
+
+  optante_simples_nacional: true
+  regime_tributacao: 0
+  regime_especial_tributacao: 0
+  incentivo_fiscal: true
+  incentivador_cultural: true
+
+}
+export interface EnderecoPrestador {
+  logradouro: string
+  numero: string
+  complemento: string
+  bairro: string
+  codigo_municipio: string
+  cidade: string
+  uf: string
+  codigo_pais: string
+  pais: string
+  cep: string
+
+}
+export interface Tomador {
+  cpf_cnpj: string
+  inscricao_municipal: string
+  nome_razao_social: string
+  fone: string
+  email: string
+
+
+}
+export interface Intermediario {
+  cpf_cnpj: string
+  nome_razao_social: string
+  inscricao_municipal: string
+
+}
+export interface Construcao_civil {
+  codigo_obra: string
+  art: string
+
+}
+export interface Servicos {
+  0: Array_Servicos
+
+}
+export interface Array_Servicos {
+  iss_retido: true
+  responsavel_retencao: 0
+  item_lista_servico: string
+  codigo_cnae: string
+  codigo_tributacao_municipio: string
+  discriminacao: string
+  codigo_municipio: string
+  codigo_pais: string
+  tipo_tributacao: 0
+  exigibilidade_iss: 0
+  codigo_municipio_incidencia: string
+  numero_processo: string
+  unidade: string
+  quantidade: 0
+
+  valores: Valores_Servicos
+
 }
 
-// export interface Nfse {
-//         id: string,
-//         created_at: string,
-//         status: string,
-//         numero: string,
-//         codigo_verificacao: string,
-//         link_url: string,
-//         data_emissao: string,
-//         ambiente: string,
-//         referencia: string,
-//         declaracao_prestacao_servico_rps_identificacao_rps: string,
-//         rps_identificacao_numero: string,
-//         rps_identificacao_serie: string
-//         rps_data_emissao: string,
-//         declaracao_prestacao_servico_competencia: string,
-//         declaracao_prestacao_servico_natureza_tributacao: string,
+export interface Valores_Servicos {
+  valor_unitario: 0
+  valor_servicos: 0
+  valor_deducoes: 0
+  valor_pis: 0
+  valor_cofins: 0
+  valor_inss: 0
+  valor_ir: 0
+  valor_csll: 0
+  valor_outras_retencoes: 0
+  valor_iss: 0
+  valor_iss_retido: 0
+  valor_liquido: 0
+  aliquota_iss: 0
+  aliquota_pis: 0
+  aliquota_cofins: 0
+  aliquota_inss: 0
+  aliquota_ir: 0
+  aliquota_csll: 0
+  desconto_incondicionado: 0
+  desconto_condicionado: 0
+}
+export interface Cancelamento {
+  id: string
+  status: "pendente"
+  data_hora: "2022-08-02T19:58:36.516Z"
+  mensagens: Mensagens_Cancelamento
+}
+export interface Mensagens_Cancelamento {
+  0: ArrayMensagens_Cancelamento
 
-//               prestador_cpf_cnpj: string,
-//               prestador_inscricao_municipal: string,
-//               prestador_nome_razao_social: string,
-//               prestador_nome_fantasia: string,
-//               prestador_email: string,
-//               prestador_endereco_logradouro: string,
-//               prestador_endereco_numero: string,
-//               prestador_endereco_bairro: string,
-//               prestador_endereco_codigo_municipio: string,
-//               prestador_endereco_cidade: string,
-//               prestador_endereco_uf: string,
-//               prestador_endereco_codigo_pais: string,
-//               prestador_endereco_pais: string,
-//               prestador_endereco_cep: string,
-//               prestador_optante_simples_nacional: string,
-//               prestador_regime_tributacao: string,
-//               prestador_regime_especial_tributacao: string,
-//               prestador_incentivo_fiscal: string,
-//               prestador_incentivador_cultural: string
+}
+export interface ArrayMensagens_Cancelamento {
+  codigo: string
+  descricao: string
+  correcao: string
+}
 
-//               tomador_cpf_cnpj: string,
-//               tomador_inscricao_municipal: string,
-//               tomador_nome_razao_social: string,
-//               tomador_fone: string,
-//               tomador_email: string,
-//               tomador_endereco_logradouro: string,
-//               tomador_endereco_numero: string,
-//               tomador_endereco_complemento: string,
-//               tomador_endereco_bairro: string,
-//               tomador_endereco_codigo_municipio: string,
-//               tomador_endereco_cidade: string,
-//               tomador_endereco_uf: string,
-//               tomador_endereco_codigo_pais: string,
-//               tomador_endereco_pais: string,
-//               tomador_endereco_cep: string,
+export interface Mensagens {
+  0: ArrayMensagens_Nfse
+}
 
-//               servicos_iss_retido: string,
-//               servicos_responsavel_retencao: string,
-//               servicos_item_lista_servico: string,
-//               servicos_codigo_cnae: string,
-//               servicos_codigo_tributacao_municipio: string,
-//               servicos_discriminacao: string,
-//               servicos_codigo_municipio: string,
-//               servicos_codigo_pais: string,
-//               servicos_tipo_tributacao: string,
-//               servicos_exigibilidade_iss: 1,
-//               servicos_codigo_municipio_incidencia: string,
-//               servicos_numero_processo: string,
-//               servicos_unidade: string,
-//               servicos_quantidade: string,
-
-//               servicos_valores_valor_unitario: string,
-//               servicos_valores_valor_servicos: string,
-//               servicos_valores_valor_deducoes: string,
-//               servicos_valores_valor_pis: string,
-//               servicos_valores_valor_cofins: string,
-//               servicos_valores_valor_inss: string,
-//               servicos_valores_valor_ir: string,
-//               servicos_valores_valor_csll: string,
-//               servicos_valores_valor_outras_retencoes: string,
-//               servicos_valores_valor_iss: string,
-//               servicos_valores_valor_iss_retido: string,
-//               servicos_valores_valor_liquido: string,
-//               servicos_valores_aliquota_iss: string,
-//               servicos_valores_aliquota_pis: string,
-//               servicos_valores_aliquota_cofins: string,
-//               servicos_valores_aliquota_inss: string,
-//               servicos_valores_aliquota_ir: string,
-//               servicos_valores_aliquota_csll: string,
-//               servicos_valores_desconto_incondicionado: string,
-//               servicos_valores_desconto_condicionado: string
-
-//         mensagens: []
-//     }
+export interface ArrayMensagens_Nfse {
+  codigo: string
+  descricao: string
+  correcao: string
+}
