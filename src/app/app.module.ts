27327e807bcdd089620bb0948d7991fc73b66689
Wagner Mobile Costa <mobile.costa@gmail.com>
2025-10-11 13:46:54 -0300

    Inclusão de import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HomeModule } from './home/home.module';
